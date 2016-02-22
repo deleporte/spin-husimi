57ba905ce847cf23282d5e8de475a4adf04cee59
Alix Deleporte <deleporte@irma-dhcp-15.u-strasbg.fr>
2016-02-22 14:45:19 +0100

    Create random 3-regular graphs
    Using Bollobas algorithm, we implemented a fast algorithm for creating the skeleton of Husimi quotients.

---
+++ random-graphs.py
@@ -0,0 +1,33 @@
+# We use the DictGraph library and an algorithm by Bollobas to create a 3-regular random graph with n vertices
+from apgl.graph import *
+from numpy import *
+
+N=10000 # must be even
+
+tries = 0
+success = False
+
+while (success == False):
+    graph= DictGraph()
+    tries += 1
+    success = True
+# First select 3N points and make a random permutation
+    perm = random.permutation(3*N)
+# Then glue them together by groups of 3
+    for i in range(0, 3*N/2):
+        vertex1 = perm[2*i]/3
+        vertex2 = perm[2*i+1]/3
+        if graph.edgeExists(vertex1, vertex2) or (vertex1 == vertex2): #We want a true graph
+            success = False
+            print "Graph failed because of double edges."
+        graph.addEdge(vertex1, vertex2)
+
+    # we then check whether the graph is connected (unlikely at large N)
+    if (len(graph.depthFirstSearch(0)) != N):
+        success = False
+        print "Graph failed because not connected"
+    print tries
+
+print "Created graph after", tries, "tries."
+#print graph.getWeightMatrix()
+#print perm    
